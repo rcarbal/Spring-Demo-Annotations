ab8f9cdb444a8d18eb8f63881dee95684d4f1b7e
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-30 10:59:41 -0700

    Field Injection

--- src/com/bibleit/springdemo/TennisCoach.java
+++ src/com/bibleit/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// Define the default constructor
@@ -13,12 +14,13 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach: inside default construcot");
 	}
 	
-	
+	/*
 	@Autowired
 	public void dosomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside doSOmeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	/*
 	@Autowired
