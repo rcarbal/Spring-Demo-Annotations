86cf4bba617ab69749c23bc688a4d5312b5f8d97
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-30 19:04:07 -0700

    Defineing Spring Beans with Java code part 1

---
+++ src/com/bibleit/springdemo/SadFortuneService.java
@@ -0,0 +1,11 @@
+package com.bibleit.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
